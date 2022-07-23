46c5328de4ac7501bea6257bd31db25171ba7240
Ojok Simon Peter <simonpeter.oj@gmail.com>
2022-07-24 02:08:52 +0300

    Learning JavaScript Fundamentals

---
+++ models/Course.js
@@ -0,0 +1,29 @@
+const course = {
+  id: 1,
+  code: '2201',
+  name: 'Computer Literacy',
+  duration: '3 Months',
+  status: 'Pending',
+  createdAt: '23/07/2022',
+  updatedAt: '23/07/2022',
+  userId: {
+    id: 1,
+    firstName: 'Ojok',
+    middleName: '',
+    lastName: 'Simon Peter',
+  },
+  instructorId: {
+    id: 1,
+    firstName: 'Ojok',
+    middleName: '',
+    lastName: 'Simon Peter',
+  },
+  studentId: {
+    id: 1,
+    firstName: 'Ojok',
+    middleName: '',
+    lastName: 'Simon Peter',
+  },
+};
+
+console.log(course);
