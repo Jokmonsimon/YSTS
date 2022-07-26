14ded5c1835b3c9b889b75f38899df0d929c13ac
Ojok Simon Peter <simonpeter.oj@gmail.com>
2022-07-26 19:24:38 +0300

    Populate Data on Broswer

--- models/script.js
+++ models/script.js
@@ -58,21 +58,62 @@ const createStudent = new Student(
   '22 July 2022 18:38:53 EST'
 );
 
-console.log('Student Object:', createStudent);
-console.log(
-  'Student Name:',
-  createStudent.surname + ' ' + createStudent.otherNames
-);
+const content = `
+<main>
+  <article>
+    <h1>${createCourse.name}</h1>
+    <hr />
+    <ul>
+      <li>ID: ${createCourse.id}</li>
+      <li>Code: ${createCourse.code}</li>
+      <li>Duration: ${createCourse.duration}</li>
+      <li>Status: ${createCourse.status}</li>
+      <li>Tye: ${createCourse.courseType}</li>
+      <li>Created At: ${createCourse.createdAt}</li>
+      <li>Updated At: ${createCourse.updatedAt}</li>
+      <li>User ID: ${createCourse.userId}</li>
+      <li>Instructor ID: ${createCourse.instructorId}</li>
+      <li>Student ID: ${createCourse.studentId}</li>
+    </ul>
+  </article>
 
-console.log('Created At:', createStudent.createdAt);
-console.log('Updated At:', createStudent.updatedAt);
+  <article>
+    <h1>Student Information</h1>
+    <hr />
+    <ul>
+      <li>Student ID: ${createStudent.id}</li>
+      <li>Full Name: ${createStudent.salutation} ${createStudent.surname} ${createStudent.otherNames}</li>
+      <li>Gender: ${createStudent.gender}</li>
+      <li>Email: ${createStudent.emailAddress}</li>
+      <li>Telephone: ${createStudent.telephone}</li>
+      <li>Date of BirthL ${createStudent.dateOfBirth}</li>
+      <li>Marital Status: ${createStudent.maritalStatus}</li>
+      <li>District: ${createStudent.disttrict}</li>
+      <li>Sub County: ${createStudent.subcounty}</li>
+      <li>Village: ${createStudent.village}</li>
+      <li>Next of Kin: ${createStudent.nextOfKinFullName}</li>
+    </ul>
+  </article>
+</main>
+`;
 
-console.log(
-  'Number of Days since Created:',
-  'Created ' + createStudent.numberOfDays() + ' days ago'
-);
+document.body.innerHTML = content;
 
-console.log(
-  'Number of Days since last Updated:',
-  'Updated ' + createStudent.daysUpdated() + ' days ago'
-);
+// console.log('Student Object:', createStudent);
+// console.log(
+//   'Student Name:',
+//   createStudent.surname + ' ' + createStudent.otherNames
+// );
+
+// console.log('Created At:', createStudent.createdAt);
+// console.log('Updated At:', createStudent.updatedAt);
+
+// console.log(
+//   'Number of Days since Created:',
+//   'Created ' + createStudent.numberOfDays() + ' days ago'
+// );
+
+// console.log(
+//   'Number of Days since last Updated:',
+//   'Updated ' + createStudent.daysUpdated() + ' days ago'
+// );
